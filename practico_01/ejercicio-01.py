d20be54737ad71ab40ff05b0b12f5005bd2ba140
tomim19 <48568855+tomim19@users.noreply.github.com>
2019-03-20 17:00:27 -0300

    Update ejercicio-01.py

--- practico_01/ejercicio-01.py
+++ practico_01/ejercicio-01.py
@@ -1,22 +1,3 @@
 #Implementar la función mayor, que reciba tres números y devuelva el mayor de ellos.
 
-a=3
-b=6
-c=2
 
-def mayor(a, b, c):
-    if a > b and a > c:
-        solucion = a
-    elif b > c:
-        solucion = b
-    else:
-        solucion = c
-    return(solucion)
-
-assert mayor(1,2,3) == 3
-assert mayor(3,2,1) == 3
-assert mayor(1,3,2) == 3
-assert mayor(3,3,3) == 3
-assert mayor(1,3,3) == 3
-assert mayor(3,1,3) == 3
-assert mayor(3,3,1) == 3
