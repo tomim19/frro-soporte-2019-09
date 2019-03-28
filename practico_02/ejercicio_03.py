f84ca6cc076f5bdde404ebbf2072cb2b68b916d8
tomim19 <48568855+tomim19@users.noreply.github.com>
2019-03-28 18:35:52 -0300

    Ejercicio-03

--- practico_02/ejercicio_03.py
+++ practico_02/ejercicio_03.py
@@ -1,3 +1,4 @@
+
 # Implementar la clase Persona que cumpla las siguientes condiciones:
 # Atributos:
 # - nombre.
@@ -9,19 +10,37 @@
 # - es_mayor_edad(): indica si es mayor de edad, devuelve un booleano.
 # - print_data(): imprime por pantalla toda la información del objeto.
 # - generar_dni(): genera un número aleatorio de 8 cifras y lo guarda dentro del atributo dni.
-
+from random import randint
 
 class Persona:
 
     def __init__(self, nombre, edad, sexo, peso, altura):
-        pass
-
+        self.n = nombre
+        self.e = edad
+        self.s = sexo
+        self.p = peso
+        self.a = altura
+        self.dni = generar_dni()
+        print_data()
     def es_mayor_edad(self):
-        pass
+        if (self.e >= 18):
+            esmayor = True
+        else:
+            esmayor = False
+        return esmayor
 
     # llamarlo desde __init__
     def generar_dni(self):
-        pass
+        doc =[]
+        for i in range (0,7):
+            append.doc = randint(0,9)
 
     def print_data(self):
-        pass
+        print(self.n)
+        print(self.e)
+        print(self.s)
+        print(self.p)
+        print(self.a)
+        print(self.dni)
+
+
