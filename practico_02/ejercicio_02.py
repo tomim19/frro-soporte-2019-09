f40f0cc7c62ce7d1c7d59f1ecf3b04d64844b5bc
tomim19 <48568855+tomim19@users.noreply.github.com>
2019-03-28 18:19:02 -0300

    ejercicio-02

--- practico_02/ejercicio_02.py
+++ practico_02/ejercicio_02.py
@@ -1,13 +1,23 @@
 # Implementar la clase Circulo que contiene un radio, y sus métodos area y perimetro.
-
+from math import pi
 
 class Circulo:
 
     def __init__(self, radio):
-        pass
+        self.r = radio
+
 
     def area(self):
-        pass
-
+        resultado1 = (self.r**2) *pi
+        return resultado1
     def perimetro(self):
-        pass
+        resultado2 = 2* pi * self.r
+        return resultado2
+
+C1 = Circulo(3)
+
+print(C1.area())
+print(C1.perimetro())
+
+assert(int(C1.area())) == 28
+assert(int(C1.perimetro())) == 18
