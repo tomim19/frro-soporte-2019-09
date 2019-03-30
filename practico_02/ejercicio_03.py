2588bfb1e574922ba5a6051bea3dfcafbff8dcd3
tomim19 <48568855+tomim19@users.noreply.github.com>
2019-03-30 12:44:21 -0300

    ejercicio03

--- practico_02/ejercicio_03.py
+++ practico_02/ejercicio_03.py
@@ -10,7 +10,9 @@
 # - es_mayor_edad(): indica si es mayor de edad, devuelve un booleano.
 # - print_data(): imprime por pantalla toda la información del objeto.
 # - generar_dni(): genera un número aleatorio de 8 cifras y lo guarda dentro del atributo dni.
+
 from random import randint
+
 
 class Persona:
 
@@ -20,8 +22,8 @@ class Persona:
         self.s = sexo
         self.p = peso
         self.a = altura
-        self.dni = generar_dni()
-        print_data()
+        self.dni = self.generar_dni()
+        self.print_data()
     def es_mayor_edad(self):
         if (self.e >= 18):
             esmayor = True
@@ -31,16 +33,20 @@ class Persona:
 
     # llamarlo desde __init__
     def generar_dni(self):
-        doc =[]
-        for i in range (0,7):
-            append.doc = randint(0,9)
+        doc = randint(10000000,99999999)
+        return doc
 
     def print_data(self):
-        print(self.n)
-        print(self.e)
-        print(self.s)
-        print(self.p)
-        print(self.a)
-        print(self.dni)
+        print("Nombre:",self.n)
+        print("Edad:",self.e)
+        print("Sexo:",self.s)
+        print("Peso:",self.p)
+        print("Altura:",self.a)
+        print("Documento:",self.dni)
+
+p1 = Persona("Tomas",22,"hombre",70,1.70)
+print("Mayor de edad?",p1.es_mayor_edad())
+
+assert(p1.es_mayor_edad()) == True
 
 
