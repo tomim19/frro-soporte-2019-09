1cf909e2a20097d9e39064caa409c328b3383e03
tomim19 <48568855+tomim19@users.noreply.github.com>
2019-03-30 13:12:26 -0300

    ejercicio-04

--- practico_02/ejercicio_04.py
+++ practico_02/ejercicio_04.py
@@ -7,16 +7,63 @@
 # Métodos:
 # - avance(): indica que porcentaje de la carrera tiene aprobada.
 # - edad_ingreso(): indica que edad tenia al ingresar a la carrera (basándose en el año actual).
+from datetime import datetime
+from random import randint
 
 
-class Estudiante:
+class Persona:
+
+    def __init__(self, nombre, edad, sexo, peso, altura):
+        self.n = nombre
+        self.e = edad
+        self.s = sexo
+        self.p = peso
+        self.a = altura
+        self.dni = self.generar_dni()
+        self.print_data()
+    def es_mayor_edad(self):
+        if (self.e >= 18):
+            esmayor = True
+        else:
+            esmayor = False
+        return esmayor
+
+    # llamarlo desde __init__
+    def generar_dni(self):
+        doc = randint(10000000,99999999)
+        return doc
+
+    def print_data(self):
+        """print("Nombre:",self.n)
+        print("Edad:",self.e)
+        print("Sexo:",self.s)
+        print("Peso:",self.p)
+        print("Altura:",self.a)
+        print("Documento:",self.dni)"""
+
+
+class Estudiante (Persona):
 
     def __init__(self, carrera, anio, cantidad_materias, cantidad_aprobadas):
-        pass
+        Persona.__init__(self,"Tomas",22,"hombre",70,1.70)
+        self.carre=carrera
+        self.an=anio
+        self.mate=cantidad_materias
+        self.apro=cantidad_aprobadas
 
     def avance(self):
-        pass
+        porcen=(self.apro/self.mate)*100
+        return porcen
+
 
     # implementar usando modulo datetime
     def edad_ingreso(self):
-        pass
+        dt = datetime.now()
+        aux = dt.year-self.an
+        edadin = self.e-aux
+        return edadin
+
+est = Estudiante ("Sistemas",2015,20,5)
+
+assert(est.avance() == 25.0)
+assert(est.edad_ingreso() == 18)
