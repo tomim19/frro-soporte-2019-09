6ca2b52d614d5a34e7cc94ffbc2133c05a80df9a
tomim19 <48568855+tomim19@users.noreply.github.com>
2019-04-04 18:03:29 -0300

    Ejercicio-01

--- practico_03/ejercicio_01.py
+++ practico_03/ejercicio_01.py
@@ -8,12 +8,16 @@
 # Implementar la funcion borrar_tabla, que borra la tabla creada anteriormente.
 
 
+import sqlite3
+
+conn = sqlite3.connect('Tabla_Ej1.db')
+c = conn.cursor()
 def crear_tabla():
-    pass
+    c.execute('''CREATE TABLE Persona(IdPersona INTEGER PRIMARY KEY AUTOINCREMENT, Nombre TEXT, FechaNacimiento TEXT,DNI INTEGER, Altura INTEGER) ''')
 
 
 def borrar_tabla():
-    pass
+    c.execute('''DROP TABLE Persona''')
 
 
 # no modificar
