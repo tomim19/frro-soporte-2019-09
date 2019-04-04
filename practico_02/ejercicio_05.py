e95c0ae4eda281329da358953a783797135643fc
tomim19 <48568855+tomim19@users.noreply.github.com>
2019-04-04 17:21:30 -0300

    Ejercicio-05

--- practico_02/ejercicio_05.py
+++ practico_02/ejercicio_05.py
@@ -3,61 +3,6 @@
 
 
 from practico_02.ejercicio_04 import Estudiante
-"""
-from random import randint
-from datetime import datetime
-
-
-class Persona:
-
-    def __init__(self, nombre, edad, sexo, peso, altura):
-        self.n = nombre
-        self.e = edad
-        self.s = sexo
-        self.p = peso
-        self.a = altura
-        self.dni = self.generar_dni()
-        self.print_data()
-    def es_mayor_edad(self):
-        if (self.e >= 18):
-            esmayor = True
-        else:
-            esmayor = False
-        return esmayor
-
-    # llamarlo desde __init__
-    def generar_dni(self):
-        doc = randint(10000000,99999999)
-        return doc
-
-    def print_data(self):
-        print("Nombre:",self.n)
-        print("Edad:",self.e)
-        print("Sexo:",self.s)
-        print("Peso:",self.p)
-        print("Altura:",self.a)
-        print("Documento:",self.dni)
-class Estudiante (Persona):
-
-    def __init__(self, carrera, anio, cantidad_materias, cantidad_aprobadas):
-        Persona.__init__(self,"Tomas",22,"hombre",70,1.70)
-        self.carre=carrera
-        self.an=anio
-        self.mate=cantidad_materias
-        self.apro=cantidad_aprobadas
-
-    def avance(self):
-        porcen=(self.apro/self.mate)*100
-        return porcen
-
-
-    # implementar usando modulo datetime
-    def edad_ingreso(self):
-        dt = datetime.now()
-        aux = dt.year-self.an
-        edadin = self.e-aux
-        return edadin 
-"""
 
 
 est1 = Estudiante ("Sistemas",2015,20,5)
