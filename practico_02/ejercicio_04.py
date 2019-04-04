27e4b1351b5ea1774103eb8ac99f3cd905fb5866
tomim19 <48568855+tomim19@users.noreply.github.com>
2019-04-04 17:20:25 -0300

    Ejercicio-04

--- practico_02/ejercicio_04.py
+++ practico_02/ejercicio_04.py
@@ -9,41 +9,7 @@
 # - edad_ingreso(): indica que edad tenia al ingresar a la carrera (basándose en el año actual).
 from practico_02.ejercicio_03 import Persona
 from datetime import datetime
-"""
-from random import randint
 
-
-class Persona:
-
-    def __init__(self, nombre, edad, sexo, peso, altura):
-        self.n = nombre
-        self.e = edad
-        self.s = sexo
-        self.p = peso
-        self.a = altura
-        self.dni = self.generar_dni()
-        self.print_data()
-    def es_mayor_edad(self):
-        if (self.e >= 18):
-            esmayor = True
-        else:
-            esmayor = False
-        return esmayor
-
-    # llamarlo desde __init__
-    def generar_dni(self):
-        doc = randint(10000000,99999999)
-        return doc
-
-    def print_data(self):
-        """print("Nombre:",self.n)
-        print("Edad:",self.e)
-        print("Sexo:",self.s)
-        print("Peso:",self.p)
-        print("Altura:",self.a)
-        print("Documento:",self.dni)"""
-
-"""
 class Estudiante (Persona):
 
     def __init__(self, carrera, anio, cantidad_materias, cantidad_aprobadas):
