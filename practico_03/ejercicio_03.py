c0a303c633c2319ad7e34032616bb68198698352
tomim19 <48568855+tomim19@users.noreply.github.com>
2019-04-25 16:04:50 -0300

    Ejercicio3

--- practico_03/ejercicio_03.py
+++ practico_03/ejercicio_03.py
@@ -2,17 +2,30 @@
 # Devuelve un booleano en base a si encontro el registro y lo borro o no.
 
 import datetime
+import sqlite3
+from Ejercicio1 import reset_tabla
+from Ejercicio2 import agregar_persona
 
-from practico_03.ejercicio_01 import reset_tabla
-from practico_03.ejercicio_02 import agregar_persona
 
+conn = sqlite3.connect('Tabla_Ej1.db')
+c = conn.cursor()
 
-def borrar_persona(id_persona):
-    return False
+def borrar_persona (id_persona):
+    sSQL = '''SELECT * FROM Persona WHERE IdPersona=?'''
+    c.execute(sSQL, (id_persona,))
+    aux = c.fetchone()
+    if aux is None:
+        return False
+    else:
+        dSQL='''DELETE FROM Persona WHERE IdPersona=?'''
+        c.execute(dSQL, (id_persona,))
+        conn.commit()
+        return True
+
 
 
 @reset_tabla
-def pruebas():
+def pruebas ():
     assert borrar_persona(agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180))
     assert borrar_persona(12345) is False
 
