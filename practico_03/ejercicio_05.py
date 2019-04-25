84bfb1fcfd121628dc959302d301a08b38b313fd
tomim19 <48568855+tomim19@users.noreply.github.com>
2019-04-25 16:07:07 -0300

    Ejercicio5

--- practico_03/ejercicio_05.py
+++ practico_03/ejercicio_05.py
@@ -2,22 +2,36 @@
 # Devuelve un booleano en base a si encontro el registro y lo actualizo o no.
 
 import datetime
+import sqlite3
 
-from practico_03.ejercicio_01 import reset_tabla
-from practico_03.ejercicio_02 import agregar_persona
-from practico_03.ejercicio_04 import buscar_persona
+from Ejercicio1 import reset_tabla
+from Ejercicio2 import agregar_persona
+from Ejercicio4 import buscar_persona
 
 
-def actualizar_persona(id_persona, nombre, nacimiento, dni, altura):
-    return False
+conn = sqlite3.connect('Tabla_Ej1.db')
+c = conn.cursor()
+
+def actualizar_persona(Id_Persona, Nombre2, FechaNacimiento2, DNI2, Altura2):
+    c.execute(
+        "UPDATE Persona SET  Nombre = ?, FechaNacimiento=?, DNI=?, Altura=? WHERE IdPersona = ?",
+        (Nombre2, FechaNacimiento2, DNI2, Altura2, Id_Persona))
+    conn.commit()
+    cSQL = '''SELECT * FROM Persona WHERE IdPersona=?'''
+    c.execute(cSQL, (Id_Persona,))
+    aux = c.fetchone()
+    if aux is None:
+        return False
+    else:
+        return aux
 
 
 @reset_tabla
 def pruebas():
-    id_juan = agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180)
-    actualizar_persona(id_juan, 'juan carlos perez', datetime.datetime(1988, 4, 16), 32165497, 181)
-    assert buscar_persona(id_juan) == (1, 'juan carlos perez', datetime.datetime(1988, 4, 16), 32165497, 181)
-    assert actualizar_persona(123, 'nadie', datetime.datetime(1988, 4, 16), 12312312, 181) is False
+    id_juan = agregar_persona('juan perez', datetime.date(1988, 5, 15), 32165498, 180)
+    actualizar_persona(id_juan, 'juan carlos perez', datetime.date(1988, 4, 16), 32165497, 181)
+    assert buscar_persona(id_juan) == (1, 'juan carlos perez', '1988-04-16', 32165497, 181)
+    assert actualizar_persona(123, 'nadie', datetime.date(1988, 4, 16), 12312312, 181) is False
 
 if __name__ == '__main__':
     pruebas()
