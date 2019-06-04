1c473a0df5c1e2822d4cd6ac7e25f75a4e553f9a
Tomas <tomim19@outlook.com>
2019-06-04 19:28:08 -0300

    Fin Practico 04

--- practico_04/ejercicio01.py
+++ practico_04/ejercicio01.py
@@ -12,154 +12,83 @@ class Aplicacion():
     def __init__(self):
         self.raiz = Tk()
         self.raiz.title("Calculadora")
-        
-        # Cambia el formato de la fuente actual a negrita para
-        # resaltar las dos etiquetas que acompañan a las cajas
-        # de entrada. (Para este cambio se ha importado el  
-        # módulo 'font' al comienzo del programa):
-        
-        fuente = font.Font(weight='bold')
-        
-        # Define las etiquetas que acompañan a las cajas de
-        # entrada y asigna el formato de fuente anterior: 
-                               
-        self.etiq1 = ttk.Label(self.raiz, text="Primer operando:", 
-                               font=fuente)
-        self.etiq2 = ttk.Label(self.raiz, text="Segundo operando:", 
-                               font=fuente)
-        
-        # Declara dos variables de tipo cadena para contener
-        # el usuario y la contraseña: 
-        
-        self.operando1 = IntVar()
-        self.operando2 = IntVar()
-        
-        # Realiza una lectura del nombre de usuario que 
-        # inició sesión en el sistema y lo asigna a la
-        # variable 'self.usuario' (Para capturar esta
-        # información se ha importando el módulo getpass
-        # al comienzo del programa):
-        
-        self.operando1.set(getpass.getuser())
-        
-        # Define dos cajas de entrada que aceptarán cadenas
-        # de una longitud máxima de 30 caracteres.
-        # A la primera de ellas 'self.ctext1' que contendrá
-        # el nombre del usuario, se le asigna la variable
-        # 'self.usuario' a la opción 'textvariable'. Cualquier
-        # valor que tome la variable durante la ejecución del
-        # programa quedará reflejada en la caja de entrada.
-        # En la segunda caja de entrada, la de la contraseña,
-        # se hace lo mismo. Además, se establece la opción
-        # 'show' con un "*" (asterisco) para ocultar la 
-        # escritura de las contraseñas:
-        
+#VARIABLES
+        self.operando1 = DoubleVar()
+        self.operando2 = DoubleVar()
+        self.total = DoubleVar()
+#LABELS        
+        self.etiq1 = ttk.Label(self.raiz, text="Primer operando:")
+        self.etiq2 = ttk.Label(self.raiz, text="Segundo operando:")
+        self.etiq3 = ttk.Label(self.raiz, text="")
+#ENTRYS
         self.ctext1 = ttk.Entry(self.raiz, 
                                 textvariable=self.operando1, 
                                 width=30)
         self.ctext2 = ttk.Entry(self.raiz, 
                                 textvariable=self.operando2, 
-                                width=30)
-        self.separ1 = ttk.Separator(self.raiz, orient=HORIZONTAL)
-        
-        # Se definen dos botones con dos métodos: El botón
-        # 'Aceptar' llamará al método 'self.aceptar' cuando
-        # sea presionado para validar la contraseña; y el botón
-        # 'Cancelar' finalizará la aplicación si se llega a
-        # presionar:
-        
-        self.boton1 = ttk.Button(self.raiz, text="Aceptar", 
-                                 command=self.aceptar)
-        self.boton2 = ttk.Button(self.raiz, text="Cancelar", 
-                                 command=quit)
+                                width=30) 
+#BOTONES  
         self.boton3 = ttk.Button(self.raiz, text="+",
                                  command=self.suma)
         self.boton4 = ttk.Button(self.raiz, text="-",
                                  command=self.resta)                         
         self.boton5 = ttk.Button(self.raiz, text="x",
                                  command=self.multip)   
-        self.boton6 = ttk.Button(self.raiz, text="/",
-                                 command=self.div)                                               
-        # Se definen las posiciones de los widgets dentro de
-        # la ventana. Todos los controles se van colocando 
-        # hacia el lado de arriba, excepto, los dos últimos, 
-        # los botones, que se situarán debajo del último 'TOP':
-        # el primer botón hacia el lado de la izquierda y el
-        # segundo a su derecha.
-        # Los valores posibles para la opción 'side' son: 
-        # TOP (arriba), BOTTOM (abajo), LEFT (izquierda)
-        # y RIGHT (derecha). Si se omite, el valor será TOP
-        # La opción 'fill' se utiliza para indicar al gestor
-        # cómo expandir/reducir el widget si la ventana cambia
-        # de tamaño. Tiene tres posibles valores: BOTH
-        # (Horizontal y Verticalmente), X (Horizontalmente) e 
-        # Y (Verticalmente). Funcionará si el valor de la opción
-        # 'expand' es True.
-        # Por último, las opciones 'padx' y 'pady' se utilizan
-        # para añadir espacio extra externo horizontal y/o 
-        # vertical a los widgets para separarlos entre sí y de 
-        # los bordes de la ventana. Hay otras equivalentes que
-        # añaden espacio extra interno: 'ipàdx' y 'ipady':
-                                         
-        self.etiq1.pack(side=TOP, fill=BOTH, expand=True, 
-                        padx=5, pady=5)
-        self.ctext1.pack(side=TOP, fill=X, expand=True, 
-                         padx=5, pady=5)
-        self.etiq2.pack(side=TOP, fill=BOTH, expand=True, 
-                        padx=5, pady=5)
-        self.ctext2.pack(side=TOP, fill=X, expand=True, 
-                         padx=5, pady=5)
-        self.separ1.pack(side=TOP, fill=BOTH, expand=True, 
-                         padx=5, pady=5)
-        self.boton1.pack(side=LEFT, fill=BOTH, expand=True, 
-                         padx=5, pady=5)
-        self.boton2.pack(side=RIGHT, fill=BOTH, expand=True, 
-                         padx=5, pady=5)
-        self.boton3.pack(side=RIGHT, fill=BOTH, expand=True, 
-                         padx=5, pady=5)
-        self.boton4.pack(side=RIGHT, fill=BOTH, expand=True, 
-                         padx=5, pady=5)
-        self.boton5.pack(side=RIGHT, fill=BOTH, expand=True, 
-                         padx=5, pady=5)
-        self.boton6.pack(side=RIGHT, fill=BOTH, expand=True, 
-                         padx=5, pady=5)
-        
-        # Cuando se inicia el programa se asigna el foco
-        # a la caja de entrada de la contraseña para que se
-        # pueda empezar a escribir directamente:
-                
-        self.ctext2.focus_set()
+        self.boton6 = ttk.Button(self.raiz, text="%",
+                                 command=self.div) 
+#GRILLA    
+        self.etiq1.grid(row=0 , column=0)
+        self.etiq2.grid(row=0 , column=1) 
+        self.etiq3.grid(row=3, column=0)                        
+        self.ctext1.grid(row=1 , column=0)                    
+        self.ctext2.grid(row=1 , column=1, padx=20)
+        self.boton3.grid(row=1 , column=3, padx=5)
+        self.boton4.grid(row=1 , column=4, padx=5)
+        self.boton5.grid(row=1 , column=5, padx=5)
+        self.boton6.grid(row=1 , column=6, padx=5) 
         
         self.raiz.mainloop()
-    
-    # El método 'aceptar' se emplea para validar la 
-    # contraseña introducida. Será llamado cuando se 
-    # presione el botón 'Aceptar'. Si la contraseña
-    # coincide con la cadena 'tkinter' se imprimirá
-    # el mensaje 'Acceso permitido' y los valores 
-    # aceptados. En caso contrario, se mostrará el
-    # mensaje 'Acceso denegado' y el foco volverá al
-    # mismo lugar.
-    
-    def aceptar(self):
-            print("Operando1:   ", self.ctext1.get())
-            print("Operando2:", self.ctext2.get())
-            self.operando1.set("")
-            self.operando2.set("")
-            self.ctext2.focus_set()
-    def suma(self):
-        resultado = self.operando1 + self.operando2
-        return resultado
-    def resta(self):
-        resultado = self.operando1 - self.operando2
-        return resultado
-    def multip(self):
-        resultado = self.operando1 * self.operando2
-        return resultado
-    def div(self):
-        resultado = self.operando1 % self.operando2
-        return resultado
+#FUNCIONES
+    def grilla2(self, opera):
+        self.ventana = Toplevel()
+        self.ventana.title(opera)
+        self.etiq1 = ttk.Label(self.ventana, text="Resultado:")
+        self.etiq2 = ttk.Label(self.ventana, textvariable=self.total)
+        self.etiq3 = ttk.Label(self.ventana, text="")
+        self.etiq4 = ttk.Label(self.ventana, text="                                                  ")
+        self.etiq1.grid(row=0 ,column=0, sticky=W)
+        self.etiq2.grid(row=0, column=2, padx=5, sticky=W)
+        self.etiq3.grid(row=1, column=0)
+        self.etiq4.grid(row=0, column=5, columnspan=5)
 
+    def suma(self):
+        op1 = float(self.operando1.get())
+        op2 = float(self.operando2.get())
+        resultado = op1 + op2
+        self.total.set(resultado)
+        self.grilla2("Suma")                  
+        
+    def resta(self):
+        op1 = float(self.operando1.get())
+        op2 = float(self.operando2.get())
+        resultado = op1 - op2
+        self.total.set(resultado)
+        self.grilla2("Resta")                    
+       
+    def multip(self):
+        op1 = float(self.operando1.get())
+        op2 = float(self.operando2.get())
+        resultado = op1 * op2
+        self.total.set(resultado)
+        self.grilla2("Multiplicacion")                  
+       
+    def div(self):
+        op1 = float(self.operando1.get())
+        op2 = float(self.operando2.get())
+        resultado = op1 / op2
+        self.total.set(resultado)
+        self.grilla2("Division")
+              
 def main():
     mi_app = Aplicacion()
     return mi_app
--- practico_04/ejercicio03.py
+++ practico_04/ejercicio03.py
@@ -1,2 +1,36 @@
 ## 3 Ejercicio Crear un Formulario que usando el control Treeview muestre la una lista con los nombre de
 ## Ciudades Argentinas y su código postal ( por lo menos 5 ciudades ) . 
+
+from tkinter import *
+from tkinter import ttk
+
+
+class Aplicacion():
+
+    def __init__(self):
+        raiz = Tk()
+        tree = ttk.Treeview(raiz)
+#Columnas
+        tree["columns"]=("one","two")
+        tree.column("#0", width=100)
+        tree.column("one", width=100 )
+        tree.column("two", width=100)
+        tree.heading("#0", text="Id")
+        tree.heading("one", text="Ciudad")
+        tree.heading("two", text="Codigo Postal")
+#Insert
+        tree.insert("" , 0,    text="id", values=("Rosario","2000"))
+        tree.insert("" , 1,    text="id", values=("Santa Fe","3000"))
+        tree.insert("" , 2,    text="id", values=("Arroyo Seco","2128"))
+        tree.insert("" , 3,    text="id", values=("Villa Constitucion","2919"))
+        tree.insert("" , 4,    text="id", values=("Rafaela","2300"))
+#Loop
+        tree.pack()
+        raiz.mainloop()
+
+def main():
+    mi_app = Aplicacion()
+    return mi_app
+
+if __name__ == '__main__':
+    main()
